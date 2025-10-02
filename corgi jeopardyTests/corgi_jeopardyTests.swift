ae1675a0a29cbc0b661f87b68834be6049581d7a
John Clem <johnnyclem@gmail.com>
2025-10-02 16:26:32 -0500

    Initial Commit

---
+++ corgi jeopardyTests/corgi_jeopardyTests.swift
@@ -0,0 +1,17 @@
+//
+//  corgi_jeopardyTests.swift
+//  corgi jeopardyTests
+//
+//  Created by John Clem on 10/2/25.
+//
+
+import Testing
+@testable import corgi_jeopardy
+
+struct corgi_jeopardyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
