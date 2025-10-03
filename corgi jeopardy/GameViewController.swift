c22ba9935b2b814849a9b97f24355492a870388f
John Clem <johnnyclem@gmail.com>
2025-10-03 16:23:03 -0500

    Implement GameBoardScene with board layout

--- corgi jeopardy/GameViewController.swift
+++ corgi jeopardy/GameViewController.swift
@@ -14,31 +14,15 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
-        }
+        guard let skView = view as? SKView else { return }
+
+        let boardScene = GameBoardScene(size: skView.bounds.size)
+        boardScene.scaleMode = .resizeFill
+        skView.presentScene(boardScene)
+
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
